33c8494b132646aefa9f45169dd428e08dbd011a
tripleee <tripleee@users.noreply.github.com>
2017-09-22 07:26:21 +0300

    app.py: simple Bottle server for DeepSpaDe
    DeepSPADE: git submodule
    requirements.txt: pip dependencies
    setup: notes from setting up the project, nominally executable

---
+++ app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from bottle import Bottle, route, request, post, app
+import keras
+from DeepSPADE import data_helpers
+
+
+app = Bottle()
+
+@app.route('/')
+def index():
+    return 'The main HTTP API is a POST request on /spam with JSON {"body": "text"}'
+
+@app.post('/spam')
+def spam():
+    body = request.json['body'][0:270]
+    filtered = data_helpers.filterinput(body)
+    score = float(app.model.predict(filtered))
+    result = round(score) == 1.0
+    return {'spam': result, 'score': score, 'text': body}
+
+
+def main():
+    from sys import argv
+    try:
+        host = argv[1]
+    except IndexError:
+        host = '0.0.0.0'
+    app.model = keras.models.load_model('save_ensemble3_1.h5')
+    app.run(host=host, port=8888)
+
+
+if __name__ == '__main__':
+    main()
